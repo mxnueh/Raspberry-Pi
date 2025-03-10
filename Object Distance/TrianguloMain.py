789b3c3856b71ef58f7148d93d031fc4dd6df0ff
JuanBeriguete <162514355+JuanBeriguete@users.noreply.github.com>
2025-03-10 18:52:13 -0400

    Add files via upload

---
+++ Object Distance/TrianguloMain.py
@@ -0,0 +1,29 @@
+from math import sqrt as rc 
+import math
+
+#saca el coseno y me lo pasa de radianes a una medida usable
+def coseno(num):
+    return (math.cos(math.radians(num)))
+    
+#potencia a la dos de manera mas exacta el programa no tiene que bregar con decimales y tiene un mayor rendimiento(truco programacion comp)    
+def pot(num):
+    return num * num
+        
+    
+#lo junta todo e implementacion de la formula de cleison     
+def func(lad1, lad2, ai, af):
+    at = abs(af - ai)
+    ladx = 0; 
+    
+    ladx = pot(lad1) + pot(lad2) - (2*((lad1)*(lad2)))*coseno(at)
+    return ladx
+    
+#organiza los datos como deben de entrar
+lista = [15, 0, 15, 90]    
+num = 0; 
+
+#ejecucion cleison 
+num = func(lista[0], lista[2], lista[1], lista[3])
+
+print(rc(num))
+     
