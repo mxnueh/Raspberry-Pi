a070b08b269aea46838a18a30a4e0afca933a06c
mxnueh <110753402+mxnueh@users.noreply.github.com>
2025-03-28 22:15:28 -0400

    Agregando la lectura del TF-Luna

--- Flask_Connection/app.py
+++ Flask_Connection/app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import time
 import threading
+from Rangefinder import getLidarData
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 TiempoTranscurrido = 0
 contador_ejecutandose = False
-
+SenalesControl = False
+LidarSensor = False
 
 @app.route('/')
 def index():
@@ -29,10 +31,28 @@ def contador():
                       'number': f"{Hours:02}:{minutes:02}:{seconds:02}:{milliseconds:03}"})
         time.sleep(0.01)  # Update every 10ms
 
+def lidar_tf_luna():
+    global LidarSensor
+    while True:
+        
+        global LidarSensor
+        
+        LidarSensor = True
+        
+        address = 0x10 # Radar default address 0x10
+        getLidarDataCmd = [0x5A,0x05,0x00,0x01,0x60] # Gets the distance value instruction
+        
+        lidar = getLidarData(address, getLidarDataCmd)
+        socketio.emit('update_lidar', {'LidarSensor': lidar})
+        
+        time.sleep(0.01)  # Update every 10ms
+
 
 @socketio.on('connect')
 def handle_connect():
     global contador_ejecutandose
+    global LidarSensor
+    
     socketio.emit('update_number', {'number': TiempoTranscurrido})
 
     if not contador_ejecutandose:
@@ -40,6 +60,10 @@ def handle_connect():
         hilo = threading.Thread(target=contador, daemon=True)
         hilo.start()
 
+    if not LidarSensor:
+        LidarSensor = True
+        hilo = threading.Thread(target=lidar_tf_luna, daemon=True)
+        hilo.start()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
